4914b4483a2c352477a4a74ee501873ef3d5e211
Julien Bernard <julien.bernard@univ-fcomte.fr>
2015-06-21 18:50:11 +0200

    add a wrapper around b2Body (to handle different floors)

---
+++ code/akgr/Body.cc
@@ -0,0 +1,61 @@
+/*
+ * Akagoria, the revenge of Kalista
+ * a single-player RPG in an open world with a top-down view.
+ *
+ * Copyright (c) 2013-2015, Julien Bernard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "Body.h"
+
+static constexpr float BOX2D_SCALE = 0.02f;
+
+namespace akgr {
+
+  Body::Body(int floor, b2Body *body)
+  : m_floor(floor)
+  , m_body(body)
+  {
+
+  }
+
+  Location Body::getLocation() const {
+    return { getPosition(), getFloor() };
+  }
+
+  int Body::getFloor() const {
+    return m_floor;
+  }
+
+  sf::Vector2f Body::getPosition() const {
+    auto pos = m_body->GetPosition();
+    return { pos.x / BOX2D_SCALE, pos.y / BOX2D_SCALE };
+  }
+
+  float Body::getAngle() const {
+    return m_body->GetAngle();
+  }
+
+  void Body::setAngleAndVelocity(float angle, float velocity) {
+    m_body->SetTransform(m_body->GetPosition(), angle);
+    m_body->SetAngularVelocity(0.0f);
+
+    b2Rot rot(angle);
+    b2Vec2 vel(0.0f, velocity * BOX2D_SCALE);
+    vel = b2Mul(rot, vel);
+
+    m_body->SetLinearVelocity(vel);
+  }
+
+}
---
+++ code/akgr/Body.h
@@ -0,0 +1,55 @@
+/*
+ * Akagoria, the revenge of Kalista
+ * a single-player RPG in an open world with a top-down view.
+ *
+ * Copyright (c) 2013-2015, Julien Bernard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef AKGR_BODY_H
+#define AKGR_BODY_H
+
+#include <Box2D/Box2D.h>
+
+#include "Location.h"
+
+namespace akgr {
+
+  class Body {
+  public:
+    Body(int floor, b2Body *body);
+
+    Body(const Body&) = delete;
+    Body& operator=(const Body&) = delete;
+
+    Body(Body&&) = default;
+    Body& operator=(Body&&) = default;
+
+    Location getLocation() const;
+
+    int getFloor() const;
+
+    sf::Vector2f getPosition() const;
+    float getAngle() const;
+
+    void setAngleAndVelocity(float angle, float velocity);
+
+  private:
+    int m_floor;
+    b2Body *m_body;
+  };
+
+}
+
+#endif // AKGR_BODY_H
